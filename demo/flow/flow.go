5ec53372146659ef827ae036ca71872aa53d9178
uljjmhn520 <284141050@qq.com>
2023-08-16 11:33:03 +0800

    fix(all): a version error

--- demo/flow/flow.go
+++ demo/flow/flow.go
@@ -62,7 +62,7 @@ func (p *remoteDemo) RemoteVersions(constraints ...*semver.Constraints) ([]*semv
 		"v1.0.0",
 		"v0.3.2",
 		"v0.3.1",
-		// "v0.3.0",
+		"v0.3.0",
 		"v0.2.2",
 		"v0.2.1",
 		"v0.2.0",
